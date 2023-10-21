36adcbf0be8bc8266f908cd58ce11999145d18d2
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-10-21 22:40:13 +0530

    HTTP Server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,24 @@
-console.log('Hi,')
+/*
+ * This file is part of Enlearn
+ * DO NOT REMOVE this banner without the express permission of the project owner.
+ */
+import express, { Application, json, raw, urlencoded } from 'express'
+import EnvConfig from './configs/env.config'
+import { FileLogger } from './utils'
+import { Server } from './base'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const app: Application = express()
+const env = new EnvConfig()
+const fileLogger = new FileLogger()
+const server = new Server(app, env.HOST, env.PORT, fileLogger)
+server.CONTROLLERS = []
+server.MIDDLEWARES = [
+	cookieParser(),
+	cors({ origin: env.ALLOWED_HOSTS }),
+	json({ limit: '10kb' }),
+	raw(),
+	urlencoded({ limit: '10kb', extended: true })
+]
+server.RUN
